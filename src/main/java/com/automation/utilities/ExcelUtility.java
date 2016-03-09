fa64382bde892634e810c60e9a7ef03ba64c7128
unknown <in02107@HW1611.cigniti.local>
2016-03-09 10:32:18 -0600

    ExcelUtility.java

--- src/main/java/com/automation/utilities/ExcelUtility.java
+++ src/main/java/com/automation/utilities/ExcelUtility.java
@@ -10,79 +10,78 @@ import java.util.List;
 public class ExcelUtility {
 
 	static FileWriter fos;
-	public static Xls_Reader xls = new Xls_Reader(
-			System.getProperty("user.dir") + "/TestData/TestData.xlsx");
-	
+	public static Xls_Reader xls = new Xls_Reader(System.getProperty("user.dir") + "/TestData/TestData.xlsx");
 
 	static final String browserConfigSheetName = "browserConfiguration";
 	static final String allTestsSheetName = "allTests";
 
-	synchronized public static void createXml(String sheetName)
-			throws IOException {
+	synchronized public static void createXml(String sheetName) throws IOException {
 
 		try {
-			System.out.println("Test Data Sheet "+xls.path+" "+xls.filename+" "+System.getProperty("user.dir"));
-			String outputFileName = System.getProperty("user.dir")
-					+ "/testng.xml";
+			System.out.println("Test Data path " + xls.path);
+			System.out.println("Test Data filename " + xls.filename);
+
+			String outputFileName = System.getProperty("user.dir") + "/testng.xml";
+			System.out.println("Output filename is " + outputFileName);
 
 			if (new File(outputFileName).exists()) {
-				System.out.println("Test suite exists in"
-						+ outputFileName);
+				System.out.println("Test suite exists in" + outputFileName);
 				new File(outputFileName).delete();
 			}
 
-			
 			fos = new FileWriter(outputFileName);
 			fos.write("<?xml version='1.0' encoding='UTF-8'?>\n");
 			fos.write("<!DOCTYPE suite SYSTEM 'http://testng.org/testng-1.0.dtd'>\n");
 			fos.write("<suite name='Mumms Automation test suite' parallel='tests' thread-count='15'>\n\n");
-			
+
 			fos.write("<parameter name='suiteExecuted' value='Regression' />\n");
 			fos.write("<parameter name='executionType' value='Sequential' />\n");
-			
-			
-			fos.write("<listeners>\n"); 
-			
+
+			fos.write("<listeners>\n");
+
 			fos.write("\t<listener class-name=\"org.uncommons.reportng.HTMLReporter\" />\n");
 			fos.write("\t<listener class-name=\"org.uncommons.reportng.JUnitXMLReporter\" />\n");
 			fos.write("\t<listener class-name=\"com.automation.utilities.AssignTestPriorityTransformer\" />\n");
-			
+
 			fos.write("</listeners>\n");
 
 			int startRow = 2;
 
 			for (int rNum = startRow; rNum <= xls.getRowCount(sheetName); rNum++) {
-				
+
 				String browser = xls.getCellData(sheetName, "Browser", rNum);
 				if ((!browser.isEmpty()) && (xls.getCellData(sheetName, "RunMode", rNum).equalsIgnoreCase("Y"))) {
-					
+
 					fos.write("\t<test name=\"" + browser + "\" preserve-order='false'>\n");
 
-
 					HashMap<String, String> browseConfigData = getBrowserDetails(browserConfigSheetName, browser);
-					fos.write("\t\t\t<parameter name=\"browser\" value=\""+ browseConfigData.get("browser") + "\"></parameter>\n");
-					fos.write("\t\t\t<parameter name=\"automationName\" value=\""+ browseConfigData.get("automationName") + "\"></parameter>\n");
-					fos.write("\t\t\t<parameter name=\"browserVersion\" value=\""+ browseConfigData.get("browserVersion") + "\"></parameter>\n");
-					fos.write("\t\t\t<parameter name=\"platformName\" value=\""+ browseConfigData.get("platformName") + "\"></parameter>\n");
-					fos.write("\t\t\t<parameter name=\"environment\" value=\""+ browseConfigData.get("environment") + "\"></parameter>\n");
-					
+					fos.write("\t\t\t<parameter name=\"browser\" value=\"" + browseConfigData.get("browser")
+							+ "\"></parameter>\n");
+					fos.write("\t\t\t<parameter name=\"automationName\" value=\""
+							+ browseConfigData.get("automationName") + "\"></parameter>\n");
+					fos.write("\t\t\t<parameter name=\"browserVersion\" value=\""
+							+ browseConfigData.get("browserVersion") + "\"></parameter>\n");
+					fos.write("\t\t\t<parameter name=\"platformName\" value=\"" + browseConfigData.get("platformName")
+							+ "\"></parameter>\n");
+					fos.write("\t\t\t<parameter name=\"environment\" value=\"" + browseConfigData.get("environment")
+							+ "\"></parameter>\n");
+
 					fos.write("\t\t <classes>\n");
 
-					List<String> allClasses = getClasses(allTestsSheetName,browser);
+					List<String> allClasses = getClasses(allTestsSheetName, browser);
 
-					for(int k = 0; k<allClasses.size(); k++){
+					for (int k = 0; k < allClasses.size(); k++) {
 
-						
 						System.out.println("class ->" + allClasses.get(k));
 						fos.write("\t\t\t<class name=\"" + allClasses.get(k) + "\">\n");
-						List<String> allMethods = getMethods(allTestsSheetName,allClasses.get(k));
+						List<String> allMethods = getMethods(allTestsSheetName, allClasses.get(k));
 						fos.write("\t\t\t\t<methods>\n");
-						String userrole = getuserrole(allTestsSheetName,allClasses.get(k));
+						String userrole = getuserrole(allTestsSheetName, allClasses.get(k));
 						if (!userrole.isEmpty())
-							fos.write("\t\t\t<parameter name=\"userrole\" value=\""	+ userrole + "\"></parameter>\n");
-						for(int l =0; l<allMethods.size(); l++){
+							fos.write("\t\t\t<parameter name=\"userrole\" value=\"" + userrole + "\"></parameter>\n");
+						for (int l = 0; l < allMethods.size(); l++) {
 							System.out.println("\t method ->" + allMethods.get(l));
-							fos.write("\t\t\t\t\t <include name=\""	+ allMethods.get(l) + "\" />\n");
+							fos.write("\t\t\t\t\t <include name=\"" + allMethods.get(l) + "\" />\n");
 						}
 						fos.write("\t\t\t\t</methods>\n");
 
@@ -96,13 +95,11 @@ public class ExcelUtility {
 
 			}
 
-
 			fos.write("</suite>");
 			fos.flush();
 
 			if (new File(outputFileName).exists()) {
-				System.out.println("Test suite successfully generated in"
-						+ outputFileName);
+				System.out.println("Test suite successfully generated in" + outputFileName);
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -110,18 +107,17 @@ public class ExcelUtility {
 
 	}
 
-
-	public static List<String> getClasses(String allTestsSheetName, String browser){
+	public static List<String> getClasses(String allTestsSheetName, String browser) {
 		List<String> classes = new ArrayList<String>();
 
 		int startRow = 2;
-		for(int i = startRow; i<xls.getRowCount(allTestsSheetName); i++){
+		for (int i = startRow; i < xls.getRowCount(allTestsSheetName); i++) {
 
-			if((xls.getCellData(allTestsSheetName, "browserToExecute", i).contains(browser)) 
-					&& (xls.getCellData(allTestsSheetName, "Runmode", i).equalsIgnoreCase("Y"))){
+			if ((xls.getCellData(allTestsSheetName, "browserToExecute", i).contains(browser))
+					&& (xls.getCellData(allTestsSheetName, "Runmode", i).equalsIgnoreCase("Y"))) {
 
 				String classname = xls.getCellData(allTestsSheetName, "classname", i);
-				classes.add(classname);	
+				classes.add(classname);
 			}
 
 		}
@@ -129,26 +125,25 @@ public class ExcelUtility {
 		return classes;
 	}
 
-
-	public static List<String> getMethods(String allTestsSheetName, String className){
+	public static List<String> getMethods(String allTestsSheetName, String className) {
 		List<String> methods = new ArrayList<String>();
 
-		
 		int startRow = 2;
-		for(int i = startRow; i<xls.getRowCount(allTestsSheetName); i++){
+		for (int i = startRow; i < xls.getRowCount(allTestsSheetName); i++) {
 
-			if((xls.getCellData(allTestsSheetName, "classname", i).equalsIgnoreCase(className)) 
-					&& (xls.getCellData(allTestsSheetName, "Runmode", i).equalsIgnoreCase("Y"))){
-				int methodNum = i+1;
-				do{
+			if ((xls.getCellData(allTestsSheetName, "classname", i).equalsIgnoreCase(className))
+					&& (xls.getCellData(allTestsSheetName, "Runmode", i).equalsIgnoreCase("Y"))) {
+				int methodNum = i + 1;
+				do {
 					String methodName = xls.getCellData(allTestsSheetName, "testmethod", methodNum);
-					if(!(methodName.isEmpty())&&(xls.getCellData(allTestsSheetName, "Runmode", methodNum).equalsIgnoreCase("Y"))){
-						
+					if (!(methodName.isEmpty())
+							&& (xls.getCellData(allTestsSheetName, "Runmode", methodNum).equalsIgnoreCase("Y"))) {
+
 						methods.add(methodName);
 					}
 					methodNum++;
 
-				}while(!(xls.getCellData(allTestsSheetName, "testmethod", methodNum).isEmpty()));
+				} while (!(xls.getCellData(allTestsSheetName, "testmethod", methodNum).isEmpty()));
 
 			}
 
@@ -157,16 +152,15 @@ public class ExcelUtility {
 		return methods;
 	}
 
-
-	public static String getuserrole(String allTestsSheetName, String className){
+	public static String getuserrole(String allTestsSheetName, String className) {
 
 		String userrole = "";
-		
-		int startRow = 2;
-		for(int i = startRow; i<xls.getRowCount(allTestsSheetName); i++){
 
-			if((xls.getCellData(allTestsSheetName, "classname", i).equalsIgnoreCase(className))){
-				userrole = xls.getCellData(allTestsSheetName, "userrole", i);		
+		int startRow = 2;
+		for (int i = startRow; i < xls.getRowCount(allTestsSheetName); i++) {
+
+			if ((xls.getCellData(allTestsSheetName, "classname", i).equalsIgnoreCase(className))) {
+				userrole = xls.getCellData(allTestsSheetName, "userrole", i);
 			}
 
 		}
@@ -174,29 +168,26 @@ public class ExcelUtility {
 		return userrole;
 	}
 
-
-
-	public static HashMap<String, String> getBrowserDetails(String browserConfigSheetName, String browser){
+	public static HashMap<String, String> getBrowserDetails(String browserConfigSheetName, String browser) {
 		HashMap<String, String> browserDetails = new HashMap<String, String>();
 
 		int startRow = 1;
-		for(int i = startRow; i<xls.getRowCount(browserConfigSheetName); i++){
+		for (int i = startRow; i < xls.getRowCount(browserConfigSheetName); i++) {
 
 			String browserShown = xls.getCellData(browserConfigSheetName, 0, i);
-			if((!(browserShown.isEmpty()) && browserShown.equalsIgnoreCase(browser))){
+			if ((!(browserShown.isEmpty()) && browserShown.equalsIgnoreCase(browser))) {
 
 				int colCount = xls.getColumnCount(browserConfigSheetName, i);
-				for(int j = 0; j<colCount; j++){
+				for (int j = 0; j < colCount; j++) {
 
-					String key = xls.getCellData(browserConfigSheetName, j, i+1);
-					//	System.out.println("key -" + key);
-					String value = xls.getCellData(browserConfigSheetName, j, i+2);
-					//	System.out.println("value -" + value);
+					String key = xls.getCellData(browserConfigSheetName, j, i + 1);
+					// System.out.println("key -" + key);
+					String value = xls.getCellData(browserConfigSheetName, j, i + 2);
+					// System.out.println("value -" + value);
 					browserDetails.put(key, value);
 				}
 
 			}
-
 
 		}
 
@@ -243,8 +234,7 @@ public class ExcelUtility {
 		boolean flag = false;
 
 		try {
-			if (xls.getCellData(sheetName, "Runmode", rNum).equalsIgnoreCase(
-					"Y"))
+			if (xls.getCellData(sheetName, "Runmode", rNum).equalsIgnoreCase("Y"))
 				flag = true;
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -256,8 +246,7 @@ public class ExcelUtility {
 	public static boolean isMethodExecutable(String sheetName, int rNum) {
 		boolean flag = false;
 		try {
-			if (xls.getCellData(sheetName, "Runmode", rNum).equalsIgnoreCase(
-					"Y"))
+			if (xls.getCellData(sheetName, "Runmode", rNum).equalsIgnoreCase("Y"))
 				flag = true;
 		} catch (Exception ex) {
 			ex.printStackTrace();
